00affcd111bf07538ea050eff3d0fb5fb49e8a4e
Aagnesh shifak <167229723+Aagneshshifak@users.noreply.github.com>
2025-09-11 13:59:18 +0530

    inital

---
+++ src/main/java/com/todo/Main.java
@@ -0,0 +1,8 @@
+package com.todo;
+import main.java.com.todo.model.Todo;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the TODO application!");
+        
+    }
+}
