b5f27660d762257a1081e65307d854110f06638f
Aagnesh shifak <167229723+Aagneshshifak@users.noreply.github.com>
2025-09-16 13:22:51 +0530

    databaseconnection

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -3,6 +3,7 @@ package com.todo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the TODO application!");
-        
+        int a= 6 ,b=9;
+        System.out.println(a+b);
     }
 }
